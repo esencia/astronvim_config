70ae1c8dc61e106a565822d9c54bede677583d7f
Javier Funes <fjfunes@trevenque.es>
2024-02-17 13:48:33 +0100

    ADD: nvim ascii art + sonokai style for colorscheme

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  colorscheme = "sonokai",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
@@ -65,6 +65,14 @@ return {
     },
   },
 
+  plugins = {
+    {
+      "sainnhe/sonokai",
+      init = function()
+        vim.g.sonokai_style = "sushia"
+      end,
+    }
+  }, 
   -- This function is run last and is a good place to configuring
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
